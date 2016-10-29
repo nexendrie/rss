e6b8d3584fb3c96498ff023ea8a2ba0af0833a05
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-10-29 09:25:00 +0200

    added extension for Nette DI

---
+++ src/Bridges/NetteDI/RssExtension.php
@@ -0,0 +1,26 @@
+<?php
+namespace Nexendrie\Rss\Bridges\NetteDI;
+use Nette\DI\CompilerExtension,
+    Nette\Utils\Validators;
+
+/**
+ * RssExtension for Nette DI Container
+ *
+ * @author Jakub Konečný
+ */
+class RssExtension extends CompilerExtension {
+  /** @var array */
+  protected $defaults = [
+    "shortenDescription" => 150
+  ];
+  
+  function loadConfiguration() {
+    $config = $this->getConfig($this->defaults);
+    Validators::assertField($config, "shortenDescription", "integer");
+    $builder = $this->getContainerBuilder();
+    $builder->addDefinition($this->prefix("generator"))
+      ->setClass(\Nexendrie\Rss\Generator::class)
+      ->addSetup("setShortenDescription", [$config["shortenDescription"]]);
+  }
+}
+?>
