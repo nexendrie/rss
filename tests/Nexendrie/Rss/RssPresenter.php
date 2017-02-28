0dc12af107decb768407978a7cbd1c962bb95416
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-02-28 15:17:44 +0100

    moved RssPresenter into namespace Nexendrie\Rss

---
+++ tests/Nexendrie/Rss/RssPresenter.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types = 1);
+
+namespace Nexendrie\Rss;
+
+/**
+ * RssPresenter
+ *
+ * @author Jakub Konečný
+ */
+class RssPresenter extends \Nette\Application\UI\Presenter {
+  /** @var \Nexendrie\Rss\Generator @inject */
+  public $generator;
+  
+  function renderDefault() {
+    $this->generator->dataSource = function() {
+      return [];
+    };
+    $this->sendResponse($this->generator->response());
+  }
+}
+?>
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,17 +3,6 @@ declare(strict_types=1);
 
 require __DIR__ . "/../vendor/autoload.php";
 
-class RssPresenter extends \Nette\Application\UI\Presenter {
-  /** @var \Nexendrie\Rss\Generator @inject */
-  public $generator;
-  
-  function renderDefault() {
-    $this->generator->dataSource = function() {
-      return [];
-    };
-    $this->sendResponse($this->generator->response());
-  }
-}
 
 Testbench\Bootstrap::setup(__DIR__ . '/_temp', function (\Nette\Configurator $configurator) {
   $configurator->addParameters(["appDir" => __DIR__,]);
