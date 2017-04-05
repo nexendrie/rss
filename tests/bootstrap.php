3239c5da519cbf9b2f4043354448309b2966259d
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-04-05 17:14:37 +0200

    removed unnecessary empty line in tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 require __DIR__ . "/../vendor/autoload.php";
 
-
 Testbench\Bootstrap::setup(__DIR__ . '/_temp', function (\Nette\Configurator $configurator) {
   $configurator->addParameters(["appDir" => __DIR__,]);
   $configurator->addConfig(__DIR__ . "/tests.neon");
