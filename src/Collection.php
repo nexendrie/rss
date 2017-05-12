fcc6d57c6fc07b0489ef95bdc6b944fb6983e948
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-05-12 18:30:56 +0200

    always assign by reference in Collection::offsetSet()

--- src/Collection.php
+++ src/Collection.php
@@ -60,7 +60,7 @@ class Collection implements \ArrayAccess, \Countable, \IteratorAggregate {
       throw new \InvalidArgumentException("Argument must be of $this->class type.");
     }
     if($index === NULL) {
-      $this->items[] = $item;
+      $this->items[] = & $item;
     } elseif($index < 0 OR $index >= count($this->items)) {
       throw new \OutOfRangeException("Offset invalid or out of range.");
     } else {
