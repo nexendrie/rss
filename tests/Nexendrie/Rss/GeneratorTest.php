1201a72d72133e2111d436ff9395e35e6356e4a5
Jakub Konečný <jakub.konecny2@centrum.cz>
2025-07-20 18:48:27 +0200

    stop ignoring some phpstan's errors they were fixed in a recent version

--- tests/Nexendrie/Rss/GeneratorTest.php
+++ tests/Nexendrie/Rss/GeneratorTest.php
@@ -244,9 +244,9 @@ final class GeneratorTest extends \Tester\TestCase {
     $info["categories"][] = new Category("def", "domain");
     $result = new \SimpleXMLElement($this->generator->generate($info));
     Assert::same("abc", (string) $result->channel->category[0]);
-    Assert::same("", (string) $result->channel->category[0]["domain"]); // @phpstan-ignore offsetAccess.notFound
+    Assert::same("", (string) $result->channel->category[0]["domain"]);
     Assert::same("def", (string) $result->channel->category[1]);
-    Assert::same("domain", (string) $result->channel->category[1]["domain"]); // @phpstan-ignore offsetAccess.notFound
+    Assert::same("domain", (string) $result->channel->category[1]["domain"]);
   }
 
   public function testCustomTemplate(): void {
