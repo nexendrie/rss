0be30342cd3d22418714e721eb373ffa68853bcc
Jakub Konečný <jakub.konecny2@centrum.cz>
2026-01-28 19:00:48 +0100

    added RSS extension DublinCore

---
+++ src/Extensions/DublinCore.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+namespace Nexendrie\Rss\Extensions;
+
+use Nexendrie\Rss\Generator;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DublinCore extends BaseExtension
+{
+    public const string ELEMENT_TITLE = "title";
+    public const string ELEMENT_CREATOR = "creator";
+    public const string ELEMENT_SUBJECT = "subject";
+    public const string ELEMENT_DESCRIPTION = "description";
+    public const string ELEMENT_PUBLISHER = "publisher";
+    public const string ELEMENT_CONTRIBUTOR = "contributor";
+    public const string ELEMENT_DATE = "date";
+    public const string ELEMENT_TYPE = "type";
+    public const string ELEMENT_FORMAT = "format";
+    public const string ELEMENT_IDENTIFIER = "identifier";
+    public const string ELEMENT_SOURCE = "source";
+    public const string ELEMENT_LANGUAGE = "language";
+    public const string ELEMENT_RELATION = "relation";
+    public const string ELEMENT_COVERAGE = "coverage";
+    public const string ELEMENT_RIGHTS = "rights";
+
+    public function getName(): string
+    {
+        return "dc";
+    }
+
+    public function getNamespace(): string
+    {
+        return "http://purl.org/dc/elements/1.1/";
+    }
+
+    public function configureChannelOptions(OptionsResolver $resolver, Generator $generator): void
+    {
+        $this->registerElements($resolver);
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_TITLE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_CREATOR), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_SUBJECT), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_DESCRIPTION), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_PUBLISHER), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_CONTRIBUTOR), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_DATE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_TYPE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_FORMAT), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_IDENTIFIER), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_SOURCE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_LANGUAGE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_RELATION), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_COVERAGE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_RIGHTS), "string");
+    }
+
+    public function configureItemOptions(OptionsResolver $resolver, Generator $generator): void
+    {
+        $this->registerElements($resolver);
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_TITLE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_CREATOR), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_SUBJECT), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_DESCRIPTION), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_PUBLISHER), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_CONTRIBUTOR), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_DATE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_TYPE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_FORMAT), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_IDENTIFIER), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_SOURCE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_LANGUAGE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_RELATION), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_COVERAGE), "string");
+        $resolver->setAllowedTypes($this->getElementName(self::ELEMENT_RIGHTS), "string");
+    }
+}
---
+++ tests/Nexendrie/Rss/Extensions/DublinCoreTest.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace Nexendrie\Rss\Extensions;
+
+use Nexendrie\Rss\Collection;
+use Nexendrie\Rss\Generator;
+use Nexendrie\Rss\RssChannelItem;
+use Tester\Assert;
+
+require __DIR__ . "/../../../bootstrap.php";
+
+/**
+ * @author Jakub Konečný
+ * @testCase
+ */
+final class DublinCoreTest extends \Tester\TestCase
+{
+    public function testGetName(): void
+    {
+        $extension = new DublinCore();
+        Assert::same("dc", $extension->getName());
+    }
+
+    public function testExtension(): void
+    {
+        $generator = new Generator();
+        $generator->extensions[] = $extension = new DublinCore();
+        $extensionName = $extension->getName();
+        $extensionNamespace = $extension->getNamespace();
+        $elementName1 = DublinCore::ELEMENT_TITLE;
+        $elementName2 = DublinCore::ELEMENT_CREATOR;
+        $info = [
+            "title" => "Nexendrie RSS", "link" => "https://gitlab.com/nexendrie/rss/",
+            "description" => "News for package nexendrie/rss",
+        ];
+        $generator->dataSource = static function () use ($extensionName, $elementName1, $elementName2) {
+            $collection = new Collection();
+            $collection[] = new RssChannelItem([
+                "title" => "Item 1", "description" => "Item 1 description", "link" => "", "pubDate" => 123,
+                "$extensionName:$elementName1" => "abc", "$extensionName:$elementName2" => "def",
+            ]);
+            return $collection;
+        };
+        $result = $generator->generate($info);
+        Assert::type("string", $result);
+        $result = new \SimpleXMLElement($result);
+        $namespaces = $result->getNamespaces(true);
+        Assert::same($extension->getNamespace(), $namespaces[$extensionName]);
+        Assert::same("abc", (string) $result->channel->item->children($extensionNamespace)->$elementName1);
+        Assert::same("def", (string) $result->channel->item->children($extensionNamespace)->$elementName2);
+    }
+}
+
+$test = new DublinCoreTest();
+$test->run();
