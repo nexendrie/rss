0333ef1e025f38cec2b83474a89dfe3c2a3f499f
Jakub Konečný <jakub.konecny2@centrum.cz>
2026-03-08 22:08:36 +0100

    added another url for CreativeCommons

--- src/Extensions/CreativeCommons.php
+++ src/Extensions/CreativeCommons.php
@@ -8,6 +8,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * @see https://www.rssboard.org/creative-commons
+ * @see http://backend.userland.com/creativeCommonsRssModule
  */
 class CreativeCommons extends BaseExtension
 {
